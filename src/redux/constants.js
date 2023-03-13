2407e1d7f59a85921ae66760aef091a6f7ee28cc
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-03-13 15:27:44 +0200

    надсилання екшенів

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,5 +1,5 @@
-export const statusFilters = {
+export const statusFilters = Object.freeze({
   all: 'all',
   active: 'active',
   completed: 'completed',
-};
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
-// import { createStore } from 'redux';
-import { legacy_createStore as createStore } from 'redux';
+import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
 import { statusFilters } from './constants';
 
-// Початкове значення стану Redux для кореневого редюсера,
-// якщо не передати параметр preloadedState.
 const initialState = {
   tasks: [
     { id: 0, text: 'Learn HTML and CSS', completed: true },
@@ -18,12 +15,9 @@ const initialState = {
   },
 };
 
-// Поки що використовуємо редюсер який
-// тільки повертає отриманий стан
 const rootReducer = (state = initialState, action) => {
   return state;
 };
 
-// Створюємо розширення стора, щоб додати інструменти розробника
 const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, enhancer);
